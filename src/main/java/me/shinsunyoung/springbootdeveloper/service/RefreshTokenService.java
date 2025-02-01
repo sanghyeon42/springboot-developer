0dbc78cb22667d021be2e7013fbf02784be87740
sanghyeon42 <songv67@gmail.com>
2025-02-01 14:50:22 +0900

    project init

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,30 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.config.jwt.TokenProvider;
+import me.shinsunyoung.springbootdeveloper.domain.RefreshToken;
+import me.shinsunyoung.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenProvider tokenProvider;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+    @Transactional
+    public void delete() {
+        String token = SecurityContextHolder.getContext().getAuthentication()
+                .getCredentials().toString();
+        Long userId = tokenProvider.getUserId(token);
+
+        refreshTokenRepository.deleteByUserId(userId);
+    }
+}
